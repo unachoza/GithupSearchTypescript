440238f0cf4bf4eebf24670463fdcfd10f38f288
unachoza <unachoza@gmail.com>
2019-06-18 12:57:52 -0400

    added ResultsList scaffolding with Typescript fixed Github fetch errors

--- src/API/Github.tsx
+++ src/API/Github.tsx
@@ -1,38 +1,3 @@
-
-
-
-// export const handleQuery = async () => {
-//     console.log(this.state);
-//     const { text, license, forked, stars } = this.state;
-//     if (text && license && stars) {
-//       console.log("waiting");
-//       await fetch(
-//         `https://api.github.com/search/repositories?q=${text}+license:${license}+stars:${stars}+fork:${forked}&sort=stars&order=desc`
-//       )
-//         .then(res => res.json())
-//         .then(data => {
-//           this.setState({
-//             isLoaded: true,
-//             data: data.items
-//           });
-//         })
-//         .catch(error => {
-//           this.setState({ error });
-//           return (
-//             <div className="content">
-//               <h1>
-//                 There was a problem: <br />
-//                 {this.state.error}
-//               </h1>
-//             </div>
-//           );
-//         });
-//     } else {
-//       console.log("no");
-//       alert("please fill all inputs");
-//     }
-//   };
-
 import axios from 'axios'
 import { async } from 'q';
 
---
+++ src/Components/ResultsList.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// interface Results {
+//     results: []
+// }
+// interface Props {
+//     results: []
+// }
+
+// class ResultsList extends React.Component{
+
+//     onButtonClick = (): void => {
+//         console.log("clicked")
+//     }
+
+//     renderList(): JSX.Element[] {
+//         return this.props.results.map((result:Results) => {
+//             return <div key={result.id}>{result.title}</div>
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>She did this {this.renderList()}</div>
+//         )
+//     }
+// }
+
+// export default ResultsList
