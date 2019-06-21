0715a8b8428f350508d975e2151c16aa426d713f
unachoza <unachoza@gmail.com>
2019-06-21 19:54:17 -0400

    mapped over Select dropdown

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -10,56 +10,53 @@ interface FormProps extends React.Props<any> {
 }
 
 const Form = (props: FormProps) => {
-    return (
-      <div className="content">
-        <form className="form" onSubmit={e => props.submit(e)}>
-          <div className="column">
-            Text
-            <br />
+  return (
+    <div className="content">
+      <form className="form" onSubmit={e => props.submit(e)}>
+        <div className="column">
+          Text
+          <br />
+          <input
+            type="input"
+            placeholder="Text"
+            required
+            onChange={e => props.text(e)}
+          />
+          <br />
+          License
+          <br />
+          <select
+            className="dropdown"
+            name="license"
+            onChange={e => props.dropDown(e)}
+          >
+            {["", "MIT", "ISC", "apache-2.0", "gpl"].map(choice => (
+              <option value={choice}>{choice}</option>
+            ))}
+          </select>
+        </div>
+        <div className="column">
+          Stars
+          <br />
+          <input
+            type="input"
+            placeholder="Stars"
+            onChange={e => props.stars(e)}
+          />{" "}
+          <br />
+          <div id="fork">
             <input
-              type="input"
-              placeholder="Text"
-              required
-              onChange={e => props.text(e)}
+              id="box"
+              type="checkbox"
+              onClick={e => props.toggleFork(e)}
             />
-            <br />
-            License
-            <br />
-            <select
-              className="dropdown"
-              name="license"
-              onChange={e => props.dropDown(e)}
-            >
-              <option value="null" />
-              <option value="MIT">MIT</option>
-              <option value="ISC">ISC</option>
-              <option value="apache-2.0">apache-2.0</option>
-              <option value="gpl">gpl</option>
-            </select>
+            <p id="checkbox-title">Include Forked</p>
           </div>
-          <div className="column">
-            Stars
-            <br />
-            <input
-              type="input"
-              placeholder="Stars"
-              onChange={e => props.stars(e)}
-            />{" "}
-            <br />
-            <div id="fork">
-              <input
-                id="box"
-                type="checkbox"
-                onClick={e => props.toggleFork(e)}
-              />
-              <p id="checkbox-title">Include Forked</p>
-            </div>
-          </div>
-          <input id="submit" type="submit" value="Search" />
-        </form>
-      </div>
-    );
-  }
-
+        </div>
+        <input id="submit" type="submit" value="Search" />
+      </form>
+    </div>
+  );
+};
 
 export default Form;
